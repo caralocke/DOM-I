c3a03612c68f17f0432ee8fba7079fab75049701
caralocke <cara.m.locke@gmail.com>
2021-05-03 16:47:37 -0400

    Was able to get the middle image to display correctly

--- js/index.js
+++ js/index.js
@@ -45,6 +45,9 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 const header = document.querySelector('header');
 const links = document.querySelectorAll('nav a');
 const logoImage = document.querySelector('#logo-img');
+const mainContent = document.querySelector('.main-content');
+const topContent = document.querySelector('.top-content:nth-of-type(1)'); 
+const topText = document.querySelector('.text-content:nth-of-type(1)');
 const ctaText = document.querySelector('h1');
 const ctaButton = document.querySelector('button');
 const ctaImage = document.querySelector('#cta-img');
@@ -54,16 +57,29 @@ const visionLink = document.createElement('a');
 const featuresLink = document.createElement('a');
 const aboutLink = document.createElement('a');
 const contactLink = document.createElement('a');
-
+const featuresTitle = document.createElement('h4');
+const featuresInfo = document.createElement('p');
+const aboutTitle = document.createElement('h4');
+const aboutInfo = document.createElement('p');
+const middleImage = document.querySelector('#middle-img');
 
 //Using your selectors, update the content to match the example file.
 ctaText.textContent = `DOM Is Awesome`;
 ctaText.style = 'black';
 ctaButton.textContent = "Get Started";
+
+
+
+//Remember to update the src attributes on images
+ctaImage.src = 'img/header-img.png';
+middleImage.src = 'img/mid-page-accent.jpg';
+
+//Change the color of the navigation text to be green.
+
+//Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system. You can call them whatever you want.
 servicesLink.textContent = 'Services';
 servicesLink.href = '#';
 document.querySelector('nav').appendChild(servicesLink);
-console.log(servicesLink);
 productLink.textContent = 'Product';
 productLink.href = '#';
 document.querySelector('nav').appendChild(productLink);
@@ -79,11 +95,15 @@ document.querySelector('nav').appendChild(aboutLink);
 contactLink.textContent = 'Contact';
 contactLink.href = '#';
 document.querySelector('nav').appendChild(contactLink);
-
-
-//Remember to update the src attributes on images
-ctaImage.src = 'img/header-img.png';
-
-//Change the color of the navigation text to be green.
-links.style.fontColor = 'green'
-//Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system. You can call them whatever you want.
+featuresTitle.textContent = 'FEATURES';
+document.querySelector('h4').appendChild(featuresTitle);
+featuresInfo.textContent = `Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,
+in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus
+scelerisque quis.`
+document.querySelector('p').appendChild(featuresInfo);
+aboutTitle.textContent = "ABOUT";
+document.querySelector('h4').appendChild(aboutTitle);
+aboutInfo.textContent = `About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in
+interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus
+scelerisque quis.`
+document.querySelector('p').appendChild(aboutInfo);
